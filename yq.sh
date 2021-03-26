0f7477e8d74a3356f3396dda5f65e24874b84cfc
lctech-zeki <zeki@lctech.com.tw>
2021-03-26 09:57:23 +0800

    add yq.sh

---
+++ yq.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+#https://mikefarah.gitbook.io/yq/
+# Use ->  cat a.yaml | sh yq.sh e '.metadata.name' -  
+
+yq-docker-run() {
+  docker run --rm -i -v "${PWD}":/workdir mikefarah/yq "$@"
+}
+
+yq-docker-run "$@"
