11f41d3b2a62aff68e601102a20ac82541ffcab9
lctech-zeki <zeki@lctech.com.tw>
2021-06-21 20:58:29 +0800

    fix

--- proto/build-protoc.sh
+++ proto/build-protoc.sh
@@ -37,7 +37,7 @@ function build() {
     echo "@ ts"
     for proto in $proto_files; do
         protoc -I=src/ -I=/opt/include "${proto}" \
-            --grpc-web_out=import_style=commonjs+dts,mode=grpcwebtext:./dist/ts \
+            --grpc-web_out=import_style=typescript,mode=grpcwebtext:./dist/ts \
             #--ts_out=./dist/ts
     done
 
