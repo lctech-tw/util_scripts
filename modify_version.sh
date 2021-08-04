289a97da2fe699cefe31d590b321145d7bf9c2c0
lctech-zeki <zeki@lctech.com.tw>
2021-08-04 09:18:24 +0800

    modfiy func

--- modify_version.sh
+++ modify_version.sh
@@ -6,35 +6,32 @@
 
 # mock testing
 function testing() {
+    echo "@ ${FUNCNAME[0]}"
     if [ ! -f ./package.json ] ; then
     echo '{"version":"1.0.21"}' >package.json
-    GITHUB_ACTIONS=true
+    #GITHUB_ACTIONS=true
     fi
 }
 
 function Update() {
-    echo "@ UpdateVersion"
-    mv package.json origin.package.json
-    VERSION_OLD=$(jq <origin.package.json '.version')
+    echo "@ ${FUNCNAME[0]}"
+    VERSION_OLD=$(jq <package.json '.version')
     VERSION_OLD_LAST=$(echo "$VERSION_OLD" | cut -f3 -d"." | cut -f1 -d'"')
     VERSION_NEW_LAST=$((VERSION_OLD_LAST + 1))
     VERSION_NEW=$(echo "$VERSION_OLD" | cut -f1,2 -d".")'.'"$VERSION_NEW_LAST"'"'
     echo "🐥 Update Version : $VERSION_OLD_LAST --> $VERSION_NEW_LAST"
     echo "🐥 New Version : $VERSION_NEW"
-    cat <origin.package.json | jq '.version'="$VERSION_NEW" >package.json
-    rm origin.package.json
-    # GITHUB_ENV ->  github actions use
+    cat <<< "$(jq '.version'="$VERSION_NEW" package.json)" > package.json
+    # GITHUB_ENV -> github actions use
     if [ "$GITHUB_ACTIONS" ]; then
         echo "TAG_VERSION=""v""$(jq -r '.version' <package.json)" >>"$GITHUB_ENV"
     fi
 }
 function RenamePackage() {
-    echo "@ RenamePackage"
-    mv package.json origin.package.json
+    echo "@ ${FUNCNAME[0]}"
     NAME_NEW=$(git config --get remote.origin.url | sed 's/.*\/\([^ ]*\/[^.]*\).*/\1/')
     echo "🐹 New Name : @$NAME_NEW"
-    cat <origin.package.json | jq '.name'='"@'"$NAME_NEW"'"' >package.json
-    rm origin.package.json
+    cat <<< "$(jq '.name'='"@'"$NAME_NEW"'"' package.json)" > package.json
 }
 
 Update
