cf70ba0a99da5f2189b7a0a4187ad038e096de47
Zeki <74172787+lctech-zeki@users.noreply.github.com>
2023-04-20 11:49:40 +0800

    feat: update

--- scan.sh
+++ scan.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-
-#https://slscan.io/en/latest/
-# Use -> scan --type go
+# Purpose: scan code
+# Author: zeki@lctech.com.tw
+# Requirments: Docekr
+# -------------------------------------------------------------------
+# Usage:
+# scan.sh --type go
+# -------------------------------------------------------------------
+# Notes:
+# Ref: https://slscan.io/en/latest/
+# -------------------------------------------------------------------
 
 scan-docker-run() {
     docker run --rm -e "WORKSPACE=$(pwd)" -e GITHUB_TOKEN -v "$(pwd):/app" shiftleft/scan scan $*
--- yq.sh
+++ yq.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-
-#https://mikefarah.gitbook.io/yq/
-# Use ->  cat a.yaml | sh yq.sh e '.metadata.name' -  
+# Purpose: Use yq to parse yaml
+# Author: zeki@lctech.com.tw
+# Requirments: Docekr
+# -------------------------------------------------------------------
+# Usage:
+# cat a.yaml | sh yq.sh e '.metadata.name' -  
+# -------------------------------------------------------------------
+# Notes:
+# Ref: https://mikefarah.gitbook.io/yq/
+# -------------------------------------------------------------------
 
 yq-docker-run() {
   docker run --rm -i -v "${PWD}":/workdir mikefarah/yq "$@"
