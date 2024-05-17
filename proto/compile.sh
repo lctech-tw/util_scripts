4a4db27b49c84e163c078a46fd879e9f8c229b3c
Zeki <74172787+lctech-zeki@users.noreply.github.com>
2024-05-17 11:41:17 +0800

    feat: Add support for compiling with Neo mode

--- proto/compile.sh
+++ proto/compile.sh
@@ -23,29 +23,42 @@ elif [ "$COMPILE_MODE" == "v3" ]; then
     SCRIPT_FILE="build-protoc3.sh"
 elif [ "$COMPILE_MODE" == "v4" ]; then
     SCRIPT_FILE="build-protoc4.sh"
+elif [ "$COMPILE_MODE" == "neo" ]; then
+    SCRIPT_FILE="build-neo.sh"
 else
     SCRIPT_FILE="build-protoc.sh"
 fi
 echo -e "@ ENV / COMPILE_MODE = ${COMPILE_MODE:-Default} : SCRIPT_FILE = ${RED}$SCRIPT_FILE${NC}"
 
-# Download script
-curl -sLJO "https://raw.githubusercontent.com/lctech-tw/util_scripts/main/proto/$SCRIPT_FILE"
-curl -sLJO "https://raw.githubusercontent.com/lctech-tw/util_scripts/main/proto/build-protoc-node.sh"
+if [ ! "$(COMPILE_MODE)" == "neo" ]; then
+    echo "--- default ---"
+    # Download script
+    curl -sLJO "https://raw.githubusercontent.com/lctech-tw/util_scripts/main/proto/$SCRIPT_FILE"
+    curl -sLJO "https://raw.githubusercontent.com/lctech-tw/util_scripts/main/proto/build-protoc-node.sh"
 
-# Auth
-if [ ! "$(whoami)" == "lctech-zeki" ]; then
-    # GCP
-    gcloud auth activate-service-account docker-puller@lc-shared-res.iam.gserviceaccount.com --key-file=.github/auth/puller.json
-    gcloud auth configure-docker -q
-    # Docker
-    cat <./.github/auth/puller.json | docker login -u _json_key --password-stdin https://asia.gcr.io
+    # Auth
+    if [ ! "$(whoami)" == "lctech-zeki" ]; then
+        # GCP
+        gcloud auth activate-service-account docker-puller@lc-shared-res.iam.gserviceaccount.com --key-file=.github/auth/puller.json
+        gcloud auth configure-docker -q
+        # Docker
+        cat <./.github/auth/puller.json | docker login -u _json_key --password-stdin https://asia.gcr.io
+    fi
+
+    # Run build-protoc via docker
+    docker pull asia.gcr.io/lc-shared-res/proto-compiler:node &
+    docker pull asia.gcr.io/lc-shared-res/proto-compiler:latest
+    docker run --rm -v "$(pwd)":/workdir asia.gcr.io/lc-shared-res/proto-compiler:latest ./"$SCRIPT_FILE" build github.com/"$GITHUB_REPOSITORY"
+    docker run --rm -v "$(pwd)":/workdir asia.gcr.io/lc-shared-res/proto-compiler:node ./build-protoc-node.sh build
+else
+    echo "--- neo ---"
+    rm -rf dist
+    cd ./src || exit
+    curl -sLJO "https://raw.githubusercontent.com/lctech-tw/util_scripts/main/proto/buf.yaml"
+    curl -sLJO "https://raw.githubusercontent.com/lctech-tw/util_scripts/main/proto/buf.gen.yaml"
+    mkdir dist
+    docker run --volume "$(pwd):/workspace" --workdir /workspace bufbuild/buf generate
+    mv dist ../dist && rm -rf buf.yaml buf.gen.yaml
 fi
-
-# Run build-protoc via docker
-docker pull asia.gcr.io/lc-shared-res/proto-compiler:node &
-docker pull asia.gcr.io/lc-shared-res/proto-compiler:latest
-docker run --rm -v "$(pwd)":/workdir asia.gcr.io/lc-shared-res/proto-compiler:latest ./"$SCRIPT_FILE" build github.com/"$GITHUB_REPOSITORY"
-docker run --rm -v "$(pwd)":/workdir asia.gcr.io/lc-shared-res/proto-compiler:node ./build-protoc-node.sh build
-
 # Remove script
 rm -f ./build-protoc*
