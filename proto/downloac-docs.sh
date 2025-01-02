be5c344475e260527daf93b82ac201767eda971f
Zeki <74172787+lctech-zeki@users.noreply.github.com>
2025-01-02 12:18:53 +0800

    feat: add script to download proto files from remote repository

---
+++ proto/downloac-docs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Note : This script is used to download proto files from remote repository
+# Download proto files from remote repository
+
+declare FOLDER_PATHS="docs/proto"
+_create_folder() {
+    if [ ! -d $FOLDER_PATHS ]; then
+        mkdir -p $FOLDER_PATHS
+    fi
+}
+
+_download_doc() {
+    echo "Downloading proto files from remote repository"
+    mkdir tmp || exit
+    grep -v '^ *#' < proto-docs.config | while IFS= read -r line; do
+        echo "$line"
+        FILENAME=$(echo "$line" | awk -F'/' '{print $NF}')
+        git clone "https://github.com/$line.git" --depth 1 tmp/"$FILENAME" || echo "Failed to clone $line"
+        cp tmp/"$FILENAME"/README.md "$FOLDER_PATHS"/"$FILENAME".md || echo "Failed to copy README.md"
+    done
+    rm -rf tmp
+}
+
+if [ -f proto-docs.config ]; then
+    _create_folder
+    _download_doc
+else
+    echo "'proto-docs.config' not found"
+    exit 1
+fi
